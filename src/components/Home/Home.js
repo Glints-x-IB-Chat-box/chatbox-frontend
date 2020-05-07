101a2ab06bbba34966d6f45840b85ae7263951fb
Frederick-88 <frederickfd88@gmail.com>
2020-05-07 14:42:52 +0700

    progress terbaru

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,14 +4,18 @@ import React from "react";
 export default function Home() {
   return (
     <div className="row mx-0">
-      <div className="col-md-4 bg-success">
-        <h1>This is Home Page</h1>
-        <h1>This is Home Page</h1>
+      <div className="col-md-4 bg-primary text-white">
+        <h1 className="text-center">ChatBoxo</h1>
+        <input
+          type="text"
+          placeholder="Search Contacts..."
+          className="w-100 h-25"
+        />
         <h1>This is Home Page</h1>
         <h1>This is Home Page</h1>
       </div>
 
-      <div className="col-md-8 bg-primary">
+      <div className="col-md-8 bg-success">
         <h1>This is Home Page</h1>
         <h1>This is Home Page</h1>
         <h1>This is Home Page</h1>
