735771c59a6884980e2fd9312238b4f2a02a4ea9
Frederick-88 <frederickfd88@gmail.com>
2020-06-13 22:19:37 +0700

    mobile only, update on app.js

--- src/components/Home/onlyMobile.js
+++ src/components/Home/onlyMobile.js
@@ -7,19 +7,21 @@ export default function onlyMobile() {
       backgroundImage: `url(${image})`,
       backgroundSize: "cover",
       backgroundPosition: "center",
-      height: "20rem",
+      height: "15rem",
     };
   };
   return (
-    <div className="container bg-light">
-      <div
-        style="rounded-circle w-100 mt-4 mb-3"
-        style={picture(imageMobile)}
-      ></div>
-      <h5 className="text-center text-justify">
-        Hello! You can check "circlemessenger.com" via website! Sorry for this
-        inconvenience, we will comeback soon in the Phone!
-      </h5>
+    <div className="container bg-light" style={{ height: "100vh" }}>
+      <div className="align-self-center">
+        <div
+          className="rounded-circle w-75 mt-4 mb-3"
+          style={picture(imageMobile)}
+        ></div>
+        <h5 className="text-center text-justify">
+          Hello! You can check "circlemessenger.com" via website! Sorry for this
+          inconvenience, we will comeback soon in the Phone!
+        </h5>
+      </div>
     </div>
   );
 }
